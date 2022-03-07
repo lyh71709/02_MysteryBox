9f90328a9fe491178ad2cd080ca973b08f140f77
lyh71709 <60583161+lyh71709@users.noreply.github.com>
2022-03-07 14:49:16 +1300

    Finished Checkpoint 1

--- MB_Compiled_Game.py
+++ MB_Compiled_Game.py
@@ -124,6 +124,46 @@ class Game:
         print(stakes)
         print(starting_balance)
 
+
+        # Initialise variables
+        self.balance = IntVar()
+
+        # Set starting balance to amount entered by user at start of game
+        self.balance.set(starting_balance)
+
+        # GUI setup
+        self.game_box = Toplevel()
+        self.game_frame = Frame(self.game_box)
+        self.game_frame.grid()
+
+        # Heading (Row 0)
+        self.heading_label = Label(self.game_frame, text="Heading", font="Ariel 24 bold", padx=10, pady=10)
+        self.heading_label.grid(row=0)
+
+        # Balance Label (Row 1)
+        self.balance_frame = Frame(self.game_frame)
+        self.balance_frame.grid(row=1)
+
+        self.balance_label = Label(self.game_frame, text="Balance...", font="Ariel 12")
+        self.balance_label.grid(row=2)
+
+        self.play_button = Button(self.game_frame, text="Gain", padx=10, pady=10, command=self.reveal_boxes)
+        self.play_button.grid(row=3)
+
+    def reveal_boxes(self):
+        # retrieve the balance from the initial function
+        current_balance = self.balance.get()
+
+        # Adjust the balance (subtract game cost and add pay out)
+        # For testing purposes, just add 2
+        current_balance += 2
+
+        # Set balance to adjusted balance
+        self.balance.set(current_balance)
+
+        # Edit label so user can see their balance
+        self.balance_label.configure(text="Balance: {}".format(current_balance))
+
 # main routine
 if __name__ == "__main__":
     root = Tk()
